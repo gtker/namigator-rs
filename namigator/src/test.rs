aacbb9c2c50428e1135734c41a4231c7a256c520
Gtker <git@gtker.com>
2023-09-17 14:47:17 +0200

    rs: Move values into const variables

--- namigator/src/test.rs
+++ namigator/src/test.rs
@@ -43,8 +43,8 @@ fn test_build(output_directory: &str, data_directory: &str) {
 fn test_pathfind(output_directory: &str) {
     let mut map = PathfindMap::new(output_directory, MAP_NAME).unwrap();
 
-    const X: f32 = 16271.025391;
-    const Y: f32 = 16845.421875;
+    const ADT_X: f32 = 16271.025391;
+    const ADT_Y: f32 = 16845.421875;
 
     assert!(!map.adt_loaded(0, 1).unwrap());
     map.load_adt(0, 1).unwrap();
@@ -52,104 +52,113 @@ fn test_pathfind(output_directory: &str) {
     map.unload_adt(0, 1).unwrap();
     assert!(!map.adt_loaded(0, 1).unwrap());
 
-    map.load_adt_at(X, Y).unwrap();
+    map.load_adt_at(ADT_X, ADT_Y).unwrap();
 
-    let heights = map.find_heights(X, Y).unwrap();
-    assert_eq!(heights.len(), 2);
-    assert_eq!(heights[0], 46.30131);
-    assert_eq!(heights[1], 35.611702);
+    const ADT_HEIGHTS: [f32; 2] = [46.30131, 35.611702];
+
+    let heights = map.find_heights(ADT_X, ADT_Y).unwrap();
+    assert_eq!(heights.len(), ADT_HEIGHTS.len());
+    assert_eq!(heights[0], ADT_HEIGHTS[0]);
+    assert_eq!(heights[1], ADT_HEIGHTS[1]);
 
     map.load_all_adts().unwrap();
 
-    const START_X: f32 = 16303.294922;
-    const START_Y: f32 = 16789.242188;
-    const START_Z: f32 = 45.219631;
-    const END_X: f32 = 16200.13648;
-    const END_Y: f32 = 16834.345703;
-    const END_Z: f32 = 37.028622;
+    const PATH_FIND_FROM: Vector3d = Vector3d {
+        x: 16303.294922,
+        y: 16789.242188,
+        z: 45.219631,
+    };
 
-    let path = map
-        .find_path(
-            Vector3d {
-                x: START_X,
-                y: START_Y,
-                z: START_Z,
-            },
-            Vector3d {
-                x: END_X,
-                y: END_Y,
-                z: END_Z,
-            },
-        )
+    const PATH_FIND_TO: Vector3d = Vector3d {
+        x: 16200.13648,
+        y: 16834.345703,
+        z: 37.028622,
+    };
+
+    let path = map.find_path(PATH_FIND_FROM, PATH_FIND_TO).unwrap();
+
+    const PATH_FIND_MAX_STEPS: usize = 5;
+
+    assert!(path.len() >= PATH_FIND_MAX_STEPS);
+
+    let (zone, area) = map
+        .get_zone_and_area_raw(ADT_X, ADT_Y, ADT_HEIGHTS[0])
         .unwrap();
 
-    assert!(path.len() >= 5);
+    const ZONE_AND_AREA: u32 = 22;
 
-    let (zone, area) = map.get_zone_and_area_raw(X, Y, 46.301323).unwrap();
+    assert_eq!(zone, ZONE_AND_AREA);
+    assert_eq!(area, ZONE_AND_AREA);
 
-    assert_eq!(zone, 22);
-    assert_eq!(area, 22);
+    const LINE_OF_SIGHT_SHOULD_FAIL_FROM: Vector3d = Vector3d {
+        x: 16268.3809,
+        y: 16812.7148,
+        z: 36.1483,
+    };
+
+    const LINE_OF_SIGHT_SHOULD_FAIL_TO: Vector3d = Vector3d {
+        x: 16266.5781,
+        y: 16782.623,
+        z: 38.5035019,
+    };
 
     let should_fail = map
-        .line_of_sight(
-            Vector3d {
-                x: 16268.3809,
-                y: 16812.7148,
-                z: 36.1483,
-            },
-            Vector3d {
-                x: 16266.5781,
-                y: 16782.623,
-                z: 38.5035019,
-            },
-        )
+        .line_of_sight(LINE_OF_SIGHT_SHOULD_FAIL_FROM, LINE_OF_SIGHT_SHOULD_FAIL_TO)
         .unwrap();
     assert!(!should_fail);
 
+    const LINE_OF_SIGHT_SHOULD_PASS_FROM: Vector3d = Vector3d {
+        x: 16873.2168,
+        y: 16926.9551,
+        z: 15.9072571,
+    };
+
+    const LINE_OF_SIGHT_SHOULD_PASS_TO: Vector3d = Vector3d {
+        x: 16987.4277,
+        y: 16950.0742,
+        z: 69.4590912,
+    };
+
     let should_pass = map
-        .line_of_sight(
-            Vector3d {
-                x: 16873.2168,
-                y: 16926.9551,
-                z: 15.9072571,
-            },
-            Vector3d {
-                x: 16987.4277,
-                y: 16950.0742,
-                z: 69.4590912,
-            },
-        )
+        .line_of_sight(LINE_OF_SIGHT_SHOULD_PASS_FROM, LINE_OF_SIGHT_SHOULD_PASS_TO)
         .unwrap();
     assert!(should_pass);
 
+    const LINE_OF_SIGHT_SHOULD_PASS_DOODAD_FROM: Vector3d = Vector3d {
+        x: 16275.6895,
+        y: 16853.9023,
+        z: 37.8341751,
+    };
+
+    const LINE_OF_SIGHT_SHOULD_PASS_DOODAD_TO: Vector3d = Vector3d {
+        x: 16987.4277,
+        y: 16950.0742,
+        z: 69.4590912,
+    };
+
     let should_pass_doodad = map
         .line_of_sight(
-            Vector3d {
-                x: 16275.6895,
-                y: 16853.9023,
-                z: 37.8341751,
-            },
-            Vector3d {
-                x: 16987.4277,
-                y: 16950.0742,
-                z: 69.4590912,
-            },
+            LINE_OF_SIGHT_SHOULD_PASS_DOODAD_FROM,
+            LINE_OF_SIGHT_SHOULD_PASS_DOODAD_TO,
         )
         .unwrap();
     assert!(should_pass_doodad);
 
-    let start = Vector3d {
+    const FIND_HEIGHT_START: Vector3d = Vector3d {
         x: 16232.7373,
         y: 16828.2734,
         z: 37.1330833,
     };
-    let stop = Vector2d {
+    const FIND_HEIGTH_STOP: Vector2d = Vector2d {
         x: 16208.6,
         y: 16830.7,
     };
+    const FIND_HEIGHT_RESULT: f32 = 36.86227;
 
-    let z = map.find_height(start, stop).unwrap();
-    assert_eq!(z, 36.86227);
+    let z = map
+        .find_height(FIND_HEIGHT_START, FIND_HEIGTH_STOP)
+        .unwrap();
+    assert_eq!(z, FIND_HEIGHT_RESULT);
 
     const POINT: Vector3d = Vector3d {
         x: 16303.294922,
